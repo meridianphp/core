e7d221fbdefef8d767fd1890236c1b31b2d169d8
Jack Polgar <jack.polgar@gmail.com>
2011-03-22 09:31:49 +1100

    Added option to not initialize loaded libraries automatically.

--- libraries/load.php
+++ libraries/load.php
@@ -28,19 +28,23 @@ class Load
 	private static $classes = array();
 	private static $models = array();
 	
-	public static function library($name)
+	public static function library($name, $init = true)
 	{
-		if(in_array($name, self::$classes)) return self::$classes[$name];
+		if($init and in_array($name, self::$classes)) return self::$classes[$name];
 		
 		if(file_exists(APPPATH.'libraries/'.strtolower($name).'.php'))
 			require_once APPPATH.'libraries/'.strtolower($name).'.php';
 		else
 			Meridian::error('Loader Error','Unable to load library: '.$name);
 		
-		self::$classes[$name] = new $name();
-		self::$classes[$name]->db = Meridian::$db;
-		
-		return self::$classes[$name];
+		if($init)
+		{
+			self::$classes[$name] = new $name();
+			self::$classes[$name]->db = Meridian::$db;
+			
+			return self::$classes[$name];
+		}
+		else return true;
 	}
 	
 	public static function helper($file)
